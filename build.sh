0bab9367bf7b04964798589a08010c9e5bbf51a6
KingBBQ <gw@communitylabs.de>
2024-12-25 15:31:25 +0100

    build script für epub

--- build.sh
+++ build.sh
@@ -7,8 +7,7 @@ cat setup.yaml > all.tmp && cat $(ls -1 *.md | sort) >> all.tmp
 
 # convert with pandoc
 pandoc all.tmp -o GamesLab-Handbuch.pdf --template eisvogel --lua-filter test/convert.lua --filter pandoc-latex-environment --pdf-engine=lualatex --from markdown
-
-pandoc all.tmp -o GamesLab-Handbuch.epub --lua-filter test/convert.lua --filter pandoc-latex-environment --pdf-engine=lualatex --from markdown
+pandoc all.tmp -o GamesLab-Handbuch.epub  --from gfm --css=style.css --resource-path=bilder --embed-resources --standalone --epub-embed-font=assets/MarioWorldPixelColor.ttf --epub-embed-font=assets/PixelifySans.ttf
 pandoc all.tmp -o GamesLab-Handbuch.docx --lua-filter test/convert.lua --filter pandoc-latex-environment --pdf-engine=lualatex --from markdown
 # pandoc all.tmp -o test/GamesLab-Handbuch.md  --lua-filter test/convert.lua --filter pandoc-latex-environment --pdf-engine=lualatex --from markdown
 
